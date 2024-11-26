313127c55e7d03e37102f82b14aa1f366b658b8b
Brandon <brandonrafael80@gmail.com>
2024-11-27 00:05:40 +0700

    fix: removing path env in deployment

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-const dotenv = require("dotenv");
-const path = require("path");
-
-dotenv.config({
-  path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`),
-});
-
 const app = require("./app");
 
 const PORT = process.env.PORT;
